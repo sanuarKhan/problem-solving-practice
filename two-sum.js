5a36b4cdb6f8ddde1b2016630407d460c080b379
sanuarkhan220 <sanuarkhan220@gmail.com>
2025-10-13 23:50:00 +0600

    hash map used to solve

--- two-sum.js
+++ two-sum.js
@@ -2,14 +2,31 @@
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] == target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+
+// console.log(twoSum([2, 5, 6, 7], 9));
+
+//hash map method
+
 function twoSum(nums, target) {
+  const map = new Map();
+
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) {
-        return [i, j];
-      }
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
     }
+    map.set(nums[i], i);
   }
+  return [];
 }
 
 console.log(twoSum([2, 5, 6, 7], 9));
