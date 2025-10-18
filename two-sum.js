f8afa8c12867d8cbcddfccae06ec9c257f10706a
sanuarkhan220 <sanuarkhan220@gmail.com>
2025-10-18 11:02:42 +0600

    brute force practice -2

--- two-sum.js
+++ two-sum.js
@@ -11,6 +11,16 @@
 //     }
 //   }
 // }
+//practice 2
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] == target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
 
 // console.log(twoSum([2, 5, 6, 7], 9));
 
